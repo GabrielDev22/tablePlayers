9a04f9a9fcacd1a0540382114172e5ad4371973a
unknown <gabrieltorresmora2@gmail.com>
2023-03-30 08:51:14 -0300

    Mejorando el proyecto y implementando algunas mejoras

--- src/main/java/com/example/jugadores/service/implementation/JugadoresServiceImplementation.java
+++ src/main/java/com/example/jugadores/service/implementation/JugadoresServiceImplementation.java
@@ -20,36 +20,50 @@ public class JugadoresServiceImplementation  implements JugadoresService {
     }
     @Override
     public List<Jugadores> getAllJugadores() {
-        log.info("Empezando getALlJugadores");
-        List<Jugadores> allJugadores = jugadoresRepository.findAll();rue)
-        log.info("Terminando getAllJugadores");
-        return allJugadores;
+            log.info("Empezando getALlJugadores");
+            List<Jugadores> allJugadores = jugadoresRepository.findAll();
+            if(allJugadores.isEmpty()){
+                log.error("No se encontro ningun jugador ");
+                return null;
+            }
+            log.info("Terminando getAllJugadores");
+            return allJugadores;
     }
 
     @Override
     public Jugadores getJugadoresById(Integer id) {
+        log.info("Empezando el getJugadoresById");
         Jugadores particularJugadores = jugadoresRepository.getReferenceById(id);
+        log.info("Terminando getJugadoresById");
         return particularJugadores;
     }
 
     @Override
     public Jugadores createJugadores(Jugadores jugadores) {
+        log.info("Empezando el createJugadores");
         if(!StringUtils.hasText(jugadores.getName()))
             return null;
         Jugadores createJugadores = jugadoresRepository.save(jugadores);
+        log.info("Terminando createJugadores");
         return createJugadores;
     }
 
     @Override
     public Jugadores updateJugadores(Jugadores jugadores) {
+        log.info("Empezando el updateJugadores");
         if (!StringUtils.hasText(jugadores.getName()) || jugadores.getId() == null || jugadores.getPosition() == null) {
             log.error("Falto algun dato");
             return null;
         }
         Jugadores updateJugadores = jugadoresRepository.save(jugadores);
+        log.info("Terminando el updateJugadores");
         return updateJugadores;
     }
 
     @Override
-    public void deleteJugadoresById(Integer id) { jugadoresRepository.deleteById(id);}
+    public void deleteJugadoresById(Integer id) {
+        log.info("Empezando la eliminacion del jugador");
+        jugadoresRepository.deleteById(id);
+        log.info("Finalizando la eliminacion del jugador");
+    }
 }
