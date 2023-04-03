d0321b29c555de66488769704e8243b38dea0e13
unknown <gabrieltorresmora2@gmail.com>
2023-04-03 09:14:16 -0300

    Agregando validaciones y try y catch a service/implementation

--- src/main/java/com/example/jugadores/service/implementation/JugadoresServiceImplementation.java
+++ src/main/java/com/example/jugadores/service/implementation/JugadoresServiceImplementation.java
@@ -20,43 +20,70 @@ public class JugadoresServiceImplementation  implements JugadoresService {
     }
     @Override
     public List<Jugadores> getAllJugadores() {
+       List<Jugadores> allJugadores = jugadoresRepository.findAll();
+        try{
             log.info("Empezando getALlJugadores");
-            List<Jugadores> allJugadores = jugadoresRepository.findAll();
+            allJugadores = jugadoresRepository.findAll();
             if(allJugadores.isEmpty()){
                 log.error("No se encontro ningun jugador ");
                 return null;
             }
-            log.info("Terminando getAllJugadores");
+            return allJugadores;
+        }catch (Exception exception){
+            log.error(exception.getMessage());
+        }
             return allJugadores;
     }
 
     @Override
     public Jugadores getJugadoresById(Integer id) {
-        log.info("Empezando el getJugadoresById");
-        Jugadores particularJugadores = jugadoresRepository.getReferenceById(id);
-        log.info("Terminando getJugadoresById");
+        Jugadores particularJugadores = null;
+        try{
+            log.info("Empezando el getJugadoresById");
+            particularJugadores = jugadoresRepository.getReferenceById(id);
+            if(particularJugadores.getId() == null){
+                log.error("No se encontro ningun jugador por ese ID");
+                return null;
+            }
+            log.info("Terminando getJugadoresById");
+            return particularJugadores;
+
+        }catch (Exception exception){
+         log.error(exception.getMessage());
+        }
         return particularJugadores;
     }
 
     @Override
-    public Jugadores createJugadores(Jugadores jugadores) {
-        log.info("Empezando el createJugadores");
-        if(!StringUtils.hasText(jugadores.getName()))
-            return null;
-        Jugadores createJugadores = jugadoresRepository.save(jugadores);
-        log.info("Terminando createJugadores");
+    public Jugadores createJugadores(Jugadores jugadores){
+        Jugadores createJugadores = null;
+        try{
+            log.info("Empezando el createJugadores");
+            if(!StringUtils.hasText(jugadores.getName())) {
+                return null;
+            }
+            createJugadores = jugadoresRepository.save(jugadores);
+            log.info("Terminando createJugadores");
+        }catch(Exception exception){
+            log.error(exception.getMessage());
+        }
         return createJugadores;
     }
 
     @Override
     public Jugadores updateJugadores(Jugadores jugadores) {
-        log.info("Empezando el updateJugadores");
-        if (!StringUtils.hasText(jugadores.getName()) || jugadores.getId() == null || jugadores.getPosition() == null) {
-            log.error("Falto algun dato");
-            return null;
+        Jugadores updateJugadores = null;
+        try{
+            log.info("Empezando el updateJugadores");
+            if (!StringUtils.hasText(jugadores.getName()) || jugadores.getId() == null || jugadores.getPosition() == null) {
+                log.error("Falto algun dato");
+                return null;
+            }
+            updateJugadores = jugadoresRepository.save(jugadores);
+            log.info("Terminando el updateJugadores");
+        }catch (Exception exception){
+            log.error(exception.getMessage());
         }
-        Jugadores updateJugadores = jugadoresRepository.save(jugadores);
-        log.info("Terminando el updateJugadores");
         return updateJugadores;
     }
 
